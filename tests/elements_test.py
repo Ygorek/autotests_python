ed79e25f4a09d2741be2b19455ceb5969422f332
Ygorek <skyparts@mail.ru>
2022-05-11 00:44:27 +0300

    2 failed, 9 passed in 81.04s (0:01:21) New tests links added

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -6,7 +6,7 @@
 import time
 import random
 
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage, LinksPage
 
 
 class TestElements:
@@ -118,10 +118,18 @@ class TestElements:
             assert right == "You have done a right click", "Don't right click"
             assert click == "You have done a dynamic click", "Don't dynamic click"
 
-    # class TestLinksPage:
-    #
-    #     def test_check_link(self, driver):
-    #
-    #
-    #
-    #     def test_broken_link(self, driver):
+    class TestLinksPage:
+
+        def test_check_link(self, driver):
+            links_page = LinksPage(driver, 'https://demoqa.com/links')
+            links_page.open()
+            href_link, current_url = links_page.check_new_tab_simple_link()
+            assert href_link == current_url, "The link is broken or url is incorrect"
+
+
+        def test_broken_link(self, driver):
+            links_page = LinksPage(driver, 'https://demoqa.com/links')
+            links_page.open()
+            response_code = links_page.check_broken_link('https://demoqa.com/bad-request')
+            assert response_code == 400, "The link works or the status code is non 400"
+
