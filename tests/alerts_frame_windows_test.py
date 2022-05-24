95299c99cc1cf2acd9df49a0b63d92f7872c0ced
Ygorek <skyparts@mail.ru>
2022-05-24 18:22:49 +0300

    master branch 24.05.2022 1 failed, 22 passed, 2 rerun in 45.98s New tests in alerts_frame_windows_test.py test_see_alert test_alert_appear_5_sec test_confirm_alert test_prompt_alert

--- tests/alerts_frame_windows_test.py
+++ tests/alerts_frame_windows_test.py
@@ -1,6 +1,6 @@
 import time
 
-from pages.alerts_frame_windows_page import BrowserWindowPage
+from pages.alerts_frame_windows_page import BrowserWindowPage, AlertsPage
 
 
 class TestAlertsFrameWindow:
@@ -17,4 +17,38 @@ class TestAlertsFrameWindow:
             new_window_page = BrowserWindowPage(driver, 'https://demoqa.com/browser-windows')
             new_window_page.open()
             text_result = new_window_page.check_opened_new_window()
-            assert text_result == 'This is a sample page', "The new window has not opened or incorrect window had opened0"
+            assert text_result == 'This is a sample page', "The new window has not opened or incorrect window had opened"
+
+        # def test_change_window_message(self, driver):
+        #     new_window_page = BrowserWindowPage(driver, 'https://demoqa.com/browser-windows')
+        #     new_window_page.open()
+        #     new_window_page.check_opened_new_window2()
+
+
+
+    class TestAlertsPage:
+
+        def test_see_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button', "You don't clicked a button"
+
+        def test_alert_appear_5_sec(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', "This alert not appeared after 5 seconds"
+
+        def test_confirm_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', "You don't selected Ok"
+
+        def test_prompt_alert(self, driver):
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            #assert alert_text == f'You entered {text}'
+            assert text in alert_text, f"The {text} don't located in alert_text"
