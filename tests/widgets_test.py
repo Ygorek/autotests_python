b2fa1ae724584c745747f74a5b9d7fcd75334a8e
Ygorek <skyparts@mail.ru>
2022-05-28 22:22:53 +0300

    master branch 28.05.2022 1 failed, 26 passed in 38.72s fix widget_test.py All test worked in headless mode.

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -9,10 +9,10 @@ class TestWidgets:
         def test_accordian(self, driver):
             accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
             accordian_page.open()
-            first_title, first_content = accordian_page.check_accordian('first')
-            second_title, second_content = accordian_page.check_accordian('second')
-            time.sleep(1)
             third_title, third_content = accordian_page.check_accordian('third')
+            second_title, second_content = accordian_page.check_accordian('second')
+            first_title, first_content = accordian_page.check_accordian('first')
+
             assert first_title == 'What is Lorem Ipsum?' and first_content > 0
             assert second_title == 'Where does it come from?' and first_content > 0
             assert third_title == 'Why do we use it?' and first_content > 0
