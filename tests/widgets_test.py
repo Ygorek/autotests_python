7e477b4db6279f8e1b8a29f9da96aba70a566343
Ygorek <skyparts@mail.ru>
2022-05-29 00:01:42 +0300

    master branch 28.05.2022 1 failed, 29 passed, 3 rerun in 53.38s fix widget_test.py added separate tests for widgets All test worked in headless mode.

--- tests/widgets_test.py
+++ tests/widgets_test.py
@@ -12,26 +12,26 @@ class TestWidgets:
             third_title, third_content = accordian_page.check_accordian('third')
             second_title, second_content = accordian_page.check_accordian('second')
             first_title, first_content = accordian_page.check_accordian('first')
-            assert first_title == 'What is Lorem Ipsum?' and first_content > 0
-            assert second_title == 'Where does it come from?' and second_content > 0
-            assert third_title == 'Why do we use it?' and third_content > 0
-
-        def test_accordian2(self, driver):
-            accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
-            accordian_page.open()
-            second_title, second_content = accordian_page.check_accordian('second')
-            assert second_title == 'Where does it come from?' and second_content > 0
+            assert first_title == 'What is Lorem Ipsum?' and first_content > 0, "Different title or no content"
+            assert second_title == 'Where does it come from?' and second_content > 0, "Different title or no content"
+            assert third_title == 'Why do we use it?' and third_content > 0, "Different title or no content"
 
         def test_accordian1(self, driver):
             accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
             accordian_page.open()
             first_title, first_content = accordian_page.check_accordian('first')
-            assert first_title == 'What is Lorem Ipsum?' and first_content > 0
+            assert first_title == 'What is Lorem Ipsum?' and first_content > 0, "Different title or no content"
+
+        def test_accordian2(self, driver):
+            accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
+            accordian_page.open()
+            second_title, second_content = accordian_page.check_accordian('second')
+            assert second_title == 'Where does it come from?' and second_content > 0, "Different title or no content"
 
         def test_accordian3(self, driver):
             accordian_page = AccordianPage(driver, 'https://demoqa.com/accordian')
             accordian_page.open()
             third_title, third_content = accordian_page.check_accordian('third')
-            assert third_title == 'Why do we use it?' and third_content > 0
+            assert third_title == 'Why do we use it?' and third_content > 0, "Different title or no content"
 
 
